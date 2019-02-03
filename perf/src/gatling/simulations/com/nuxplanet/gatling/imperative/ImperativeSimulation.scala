e23b97e22d9610265f33254e3a7c564dcda4a1de
Wojciech Olech <woj.olech@gmail.com>
2019-02-03 18:20:23 +0100

    Added Docker and AWS configuration

--- perf/src/gatling/simulations/com/nuxplanet/gatling/imperative/ImperativeSimulation.scala
+++ perf/src/gatling/simulations/com/nuxplanet/gatling/imperative/ImperativeSimulation.scala
@@ -5,7 +5,7 @@ import com.nuxplanet.gatling.BaseSimulation
 class ImperativeSimulation extends BaseSimulation {
   scenarioName = "imperative"
   numberOfUsers = 1000
-  url = "http://localhost:8081"
+  url = "http://3.121.222.50:8081"
   action = "/imperative/item"
 
   run()
--- perf/src/gatling/simulations/com/nuxplanet/gatling/reactive/ReactiveSimulation.scala
+++ perf/src/gatling/simulations/com/nuxplanet/gatling/reactive/ReactiveSimulation.scala
@@ -5,7 +5,7 @@ import com.nuxplanet.gatling.BaseSimulation
 class ReactiveSimulation extends BaseSimulation{
   scenarioName = "reactive"
   numberOfUsers = 1000
-  url = "http://localhost:8090"
+  url = "http://3.121.235.198:8081"
   action = "/reactive/item"
 
   run()
